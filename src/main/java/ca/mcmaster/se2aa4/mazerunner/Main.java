5d961a830436d2ba7df0fa3307d613d18ace7f3b
Leyla Kutlu <kutlul1@mcmaster.ca>
2025-01-22 12:45:12 -0500

    Step #1: Introduced a new dependency

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -0,0 +1,48 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("** Starting Maze Runner");
+        Options options = new Options();
+        options.addOption("i", "input", true, "Maze file to read");
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            String inputFile = cmd.getOptionValue("i");
+
+            logger.info("**** Reading the maze from file " + inputFile);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (int idx = 0; idx < line.length(); idx++) {
+                    if (line.charAt(idx) == '#') {
+                        logger.info("WALL "); 
+                    } else if (line.charAt(idx) == ' ') {
+                        logger.info("PASS "); //logger
+                    }
+                }
+                System.out.print(System.lineSeparator());
+            }
+        } catch(Exception e) {
+            logger.error("/!\\ An error has occured /!\\");
+        }
+        logger.info("**** Computing path");
+        logger.warn("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
+
+        //trace < debug < info < warn < error < fatal
+    }
+}
