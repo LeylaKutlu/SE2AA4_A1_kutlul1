24bd30ef88265691f16cb34194470ea5fc5f9d8a
Leyla Kutlu <kutlul1@mcmaster.ca>
2025-01-24 14:06:47 -0500

    Implement direction for compass (#12)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Compass.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Compass.java
@@ -8,3 +8,30 @@ import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.*;
 import java.util.List;
 import java.util.ArrayList;
+
+public class Compass {
+    private static final Logger logger = LogManager.getLogger();
+    protected int row;
+    protected int col;
+    protected int direction;
+    private static final int[][] DIRECTIONS = {
+        {-1, 0},
+        {0, 1},
+        {1, 0},
+        {0, -1}
+    };
+
+    public Compass(int initialRow, int initialCol, int initialDirection) {
+        this.row = initialRow;
+        this.col = initialCol;
+        this.direction = initialDirection;
+    }
+
+    public int[] getCoordinates() {
+        return new int[]{row, col};
+    }
+
+    public int[] getDirection() {
+        return DIRECTIONS[direction];
+    }
+}
